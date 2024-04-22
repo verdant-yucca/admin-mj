e403d0a6dc218a891ce60938181ba382f32a690d
Roman Davydenko <Deadshot323rd@yandex.ru>
2024-04-22 20:59:35 +0800

    feat: add account midjourney

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,15 +4,14 @@ import { useStore } from 'effector-react';
 import { EditableCell } from '@/pages/Transactions/EditableCell';
 import { transactionsStores, transactionsEvents } from '@/shared/model/transactions';
 import { Title } from '@/pages/Users/styles';
+import { accountsEvents, accountsStores } from '@/shared/model/accountMidjourney';
 
 export const TransactionsPage = () => {
     const transactions = useStore(transactionsStores.transactions);
+    const accounts = useStore(accountsStores.accounts);
     const [form] = Form.useForm();
     const [editingKey, setEditingKey] = useState('');
     const [transactionsState, setTransactionsState] = useState(transactions);
-    const quinesClient1 = transactionsState.filter(({ midjourneyClientId }) => midjourneyClientId === '1');
-    const quinesClient2 = transactionsState.filter(({ midjourneyClientId }) => midjourneyClientId === '2');
-    const quinesClient3 = transactionsState.filter(({ midjourneyClientId }) => midjourneyClientId === '3');
 
     const isEditing = (record: any) => record.key === editingKey;
 
@@ -54,6 +53,9 @@ export const TransactionsPage = () => {
     useEffect(() => {
         transactionsEvents.getTransactionsFn();
     }, []);
+    useEffect(() => {
+        accountsEvents.getAvailableAccountMidjourneyFn();
+    }, []);
 
     useEffect(() => {
         setTransactionsState(transactions);
@@ -65,89 +67,94 @@ export const TransactionsPage = () => {
                 Если зависло в статусе running, то нажми Edit и напиши waiting start <br />
                 Если записло в статусе waiting start, то нажми Edit и напиши badRequest
             </Title>
-            {[quinesClient1, quinesClient2, quinesClient3].map((quinesClient, index) => (
-                <div
-                    style={{
-                        backgroundColor: '#bac4d8',
-                        borderRadius: '30px',
-                        padding: '2px 20px 20px',
-                        margin: '0 16px 16px 16px'
-                    }}
-                    key={index}
-                >
-                    <Title>
-                        Клиент {index + 1} ({index === 0 ? 'Jetirock' : index === 1 ? 'Bugakill' : 'deadshot'}). Ожидают
-                        запуска: {quinesClient.filter(({ stage }) => stage === 'waiting start').length}, Запущено:{' '}
-                        {quinesClient.filter(({ stage }) => stage === 'running').length}
-                    </Title>
-                    <Table
-                        dataSource={quinesClient}
-                        rowClassName="editable-row"
-                        bordered
-                        components={{
-                            body: {
-                                cell: EditableCell
-                            }
+            {accounts.map(({ customId, name }, index) => {
+                const quinesClient = transactionsState.filter(
+                    ({ midjourneyClientId }) => midjourneyClientId === customId
+                );
+                return (
+                    <div
+                        style={{
+                            backgroundColor: '#bac4d8',
+                            borderRadius: '30px',
+                            padding: '2px 20px 20px',
+                            margin: '0 16px 16px 16px'
                         }}
+                        key={index}
                     >
-                        <Table.Column key="N" dataIndex="N" title="№" render={(_, __, index) => <>{index + 1}</>} />
-                        <Table.Column key="dateQuery" dataIndex="dateQuery" title="Дата запроса" />
-                        <Table.Column key="leadTime" dataIndex="leadTime" title="Время выполнения" />
-                        <Table.Column
-                            key="prompt"
-                            dataIndex="prompt"
-                            title="prompt"
-                            onCell={record => ({
-                                inputType: 'text',
-                                dataIndex: 'prompt',
-                                title: 'prompt',
-                                editing: isEditing(record)
-                            })}
-                        />
-                        <Table.Column key="action" dataIndex="action" title="Действие" />
-                        <Table.Column
-                            key="stage"
-                            dataIndex="stage"
-                            title="Этап выполнения"
-                            onCell={record => ({
-                                inputType: 'text',
-                                dataIndex: 'stage',
-                                title: 'stage',
-                                editing: isEditing(record)
-                            })}
-                        />
-                        <Table.Column
-                            key="edit"
-                            dataIndex="edit"
-                            title="Edit"
-                            render={(_: any, record: any) => {
-                                const editable = isEditing(record);
-                                return editable ? (
-                                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                        <Typography.Link
-                                            onClick={() => save(record.key)}
-                                            style={{ margin: 0, marginBottom: 4, fontSize: '12px' }}
-                                        >
-                                            Save
-                                        </Typography.Link>
-                                        <Typography.Link onClick={cancel}>
-                                            <a style={{ margin: 0, fontSize: '12px' }}>Cancel</a>
-                                        </Typography.Link>
-                                    </div>
-                                ) : (
-                                    <Typography.Link
-                                        disabled={editingKey !== ''}
-                                        onClick={() => edit(record)}
-                                        style={{ fontSize: '12px' }}
-                                    >
-                                        Edit
-                                    </Typography.Link>
-                                );
+                        <Title>
+                            Клиент {index + 1} ({name}). Ожидают запуска:{' '}
+                            {quinesClient.filter(({ stage }) => stage === 'waiting start').length}, Запущено:{' '}
+                            {quinesClient.filter(({ stage }) => stage === 'running').length}
+                        </Title>
+                        <Table
+                            dataSource={quinesClient}
+                            rowClassName="editable-row"
+                            bordered
+                            components={{
+                                body: {
+                                    cell: EditableCell
+                                }
                             }}
-                        />
-                    </Table>
-                </div>
-            ))}
+                        >
+                            <Table.Column key="N" dataIndex="N" title="№" render={(_, __, index) => <>{index + 1}</>} />
+                            <Table.Column key="dateQuery" dataIndex="dateQuery" title="Дата запроса" />
+                            <Table.Column key="leadTime" dataIndex="leadTime" title="Время выполнения" />
+                            <Table.Column
+                                key="prompt"
+                                dataIndex="prompt"
+                                title="prompt"
+                                onCell={record => ({
+                                    inputType: 'text',
+                                    dataIndex: 'prompt',
+                                    title: 'prompt',
+                                    editing: isEditing(record)
+                                })}
+                            />
+                            <Table.Column key="action" dataIndex="action" title="Действие" />
+                            <Table.Column
+                                key="stage"
+                                dataIndex="stage"
+                                title="Этап выполнения"
+                                onCell={record => ({
+                                    inputType: 'text',
+                                    dataIndex: 'stage',
+                                    title: 'stage',
+                                    editing: isEditing(record)
+                                })}
+                            />
+                            <Table.Column
+                                key="edit"
+                                dataIndex="edit"
+                                title="Edit"
+                                render={(_: any, record: any) => {
+                                    const editable = isEditing(record);
+                                    return editable ? (
+                                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                            <Typography.Link
+                                                onClick={() => save(record.key)}
+                                                style={{ margin: 0, marginBottom: 4, fontSize: '12px' }}
+                                            >
+                                                Save
+                                            </Typography.Link>
+                                            <Typography.Link onClick={cancel}>
+                                                <a style={{ margin: 0, fontSize: '12px' }}>Cancel</a>
+                                            </Typography.Link>
+                                        </div>
+                                    ) : (
+                                        <Typography.Link
+                                            disabled={editingKey !== ''}
+                                            onClick={() => edit(record)}
+                                            style={{ fontSize: '12px' }}
+                                        >
+                                            Edit
+                                        </Typography.Link>
+                                    );
+                                }}
+                            />
+                        </Table>
+                    </div>
+                );
+            })}
         </Form>
     );
 };
